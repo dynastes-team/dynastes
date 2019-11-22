Fix bug in melspec calculation, improved numerical precision using upsampled stft-melspec conversion in test

dynastes/ops/spectral_ops.py
@@ -124,9 +124,10 @@ def stfts_to_melspecgrams(stfts: tf.Tensor, l2mel, ifreq=True) -> tf.Tensor:
     stfts = tf.transpose(stfts, perm=perm)
     stfts = tf.expand_dims(stfts, axis=-1)  # [..., channels, time, freq, 1]
     melspecgrams = _stfts_to_melspecgrams(stfts, l2mel=l2mel, ifreq=ifreq)  # [..., channels, time, freq, 2]
+
     melspecgrams_shape = melspecgrams.shape.as_list()
     perm = list(range(len(melspecgrams_shape)))
-    perm = perm[:-4] + [perm[-2], perm[-3], perm[-4], perm[-1]]
+    perm = perm[:-4] + [perm[-3], perm[-2], perm[-4], perm[-1]]
     melspecgrams = tf.transpose(melspecgrams, perm=perm)  # [..., time, freq, channels, 2]
     melspecgrams_shape = melspecgrams.shape.as_list()
     melspecgrams = tf.reshape(melspecgrams, melspecgrams_shape[:-2] + [
@@ -202,7 +203,7 @@ def _stfts_to_waves(stfts, n_fft=512, hop_length=256, discard_dc=True, pad_l=128
         frame_length=n_fft,
         frame_step=hop_length,
         fft_length=n_fft,
-        window_fn=inverse_stft_window_fn(frame_step=n_fft))
+        window_fn=inverse_stft_window_fn(frame_step=hop_length))
     waves_resyn = tf.linalg.matrix_transpose(waves_resyn)
     crops = np.reshape(np.asarray([0, 0] * (len(stfts_shape) - 3)), (-1, 2)).tolist() + [[pad_l, pad_r], [0, 0]]
     return d_array_ops.crop(waves_resyn, crops)
@@ -325,13 +326,13 @@ def _melspecgrams_to_stfts(melspecgrams, mel2l, ifreq=True):
         """
     logmelmag, mel_p = tf.unstack(melspecgrams, axis=-1)
 
-    mag = tf.matmul(tf.exp(logmelmag), mel2l, transpose_a=True)
+    mag = tf.matmul(tf.exp(logmelmag), mel2l)
     if ifreq:
         mel_phase_angle = tf.cumsum(mel_p * np.pi, axis=-2)
     else:
         mel_phase_angle = mel_p * np.pi
 
-    phase_angle = tf.matmul(mel_phase_angle, mel2l, transpose_a=True)
+    phase_angle = tf.matmul(mel_phase_angle, mel2l)
 
     return tf.expand_dims(polar2rect(mag, phase_angle), axis=-1)
 

tests/spectral_ops_test.py
@@ -9,9 +9,14 @@ from dynastes.ops import spectral_ops
 class SpectralOpsTest(tf.test.TestCase):
     @test_util.use_deterministic_cudnn
     def test_simple(self):
-        n_fft = 256
+
+        print('### Running spectral ops tests ###')
+
+        n_fft = 1024
+        fft_bins = n_fft // 2
         hop_length = 256
         pad = 8192 * 4
+        mel_upsampling = 8
         dir_path = os.path.dirname(os.path.realpath(__file__))
 
         raw_audio = tf.io.read_file(dir_path + '/data/test_wav.wav')
@@ -22,37 +27,63 @@ class SpectralOpsTest(tf.test.TestCase):
         waves = tf.squeeze(waves, axis=-1)
 
         stfts = spectral_ops.waves_to_stfts(waves, n_fft=n_fft, hop_length=hop_length, pad_l=pad, pad_r=pad,
-                                            discard_dc=False)
+                                            discard_dc=True)
         stfts_mags = tf.maximum(tf.minimum(tf.abs(stfts), 0.5), -0.5)
         stfts_angles = tf.math.angle(stfts)
         stfts = spectral_ops.polar2rect(stfts_mags, stfts_angles)
 
         waves_recon = spectral_ops.stfts_to_waves(stfts, n_fft=n_fft, hop_length=hop_length, pad_l=pad, pad_r=pad,
-                                                  discard_dc=False)
-        assert (tf.abs(tf.reduce_mean(tf.abs(waves) - tf.abs(waves_recon))).numpy() < 0.1)
+                                                  discard_dc=True)
+        wave_err = tf.abs(tf.reduce_mean(tf.abs(waves) - tf.abs(waves_recon))).numpy()
+        wave_dc_err = tf.abs(tf.reduce_mean(waves - waves_recon)).numpy()
+        print('wave_err', wave_err, 'wave_dc_err', wave_dc_err)
+        assert (wave_err < 0.03)
+        assert (wave_dc_err < 0.001)
         waves_recon_stft = spectral_ops.waves_to_stfts(waves_recon, n_fft=n_fft, hop_length=hop_length, pad_l=pad,
-                                                       pad_r=pad, discard_dc=False)
-        assert (tf.abs(tf.reduce_mean(tf.abs(stfts) - tf.abs(waves_recon_stft))).numpy() < 0.1)
-        l2mel = tf.signal.linear_to_mel_weight_matrix(num_spectrogram_bins=(n_fft // 2) + 1, num_mel_bins=96)
-
+                                                       pad_r=pad, discard_dc=True)
+        stft_err = tf.abs(tf.reduce_mean(tf.abs(stfts) - tf.abs(waves_recon_stft))).numpy()
+        stft_mean_err = tf.abs(tf.reduce_mean(stfts - waves_recon_stft)).numpy()
+        print('stft_err', stft_err, 'stft_mean_err', stft_mean_err)
+        assert (stft_err < 0.005)
+        assert (stft_mean_err < 1e-4)
+        l2mel = tf.signal.linear_to_mel_weight_matrix(num_spectrogram_bins=fft_bins, num_mel_bins=(fft_bins * mel_upsampling))
         # Mel transform
         melspecgrams = spectral_ops.stfts_to_melspecgrams(stfts, l2mel=l2mel)
+
+        # Using mel-upsampling yields much lower reconstruction error!
+        if mel_upsampling > 1:
+            melspecgrams = tf.keras.layers.AveragePooling2D((1, mel_upsampling), strides=(1, mel_upsampling), padding='same', data_format='channels_last')(melspecgrams)
+            # Simulate output downsampling
+            melspecgrams = tf.keras.layers.UpSampling2D((1, mel_upsampling), data_format='channels_last', interpolation='bilinear')(melspecgrams)
+
         stfts_mel_recon = spectral_ops.melspecgrams_to_stfts(melspecgrams, mel2l=tf.transpose(l2mel))
+        waves_mel_recon = spectral_ops.stfts_to_waves(stfts_mel_recon, n_fft=n_fft, hop_length=hop_length, discard_dc=True, pad_l=pad, pad_r=pad)
+        melspec_wav_recon_err = tf.abs(tf.reduce_mean(waves - waves_mel_recon)).numpy()
+        print('melspec_wav_recon_err', melspec_wav_recon_err)
+        assert(melspec_wav_recon_err < 0.0002)
+
+
         melspecgram_stfts_mel_recon = spectral_ops.stfts_to_melspecgrams(stfts_mel_recon, l2mel=l2mel)
 
-        assert (tf.abs(tf.reduce_mean(tf.abs(melspecgrams) - tf.abs(melspecgram_stfts_mel_recon))).numpy() < 0.4)
+        melspecgrams_err = tf.abs(tf.reduce_mean(tf.abs(melspecgrams) - tf.abs(melspecgram_stfts_mel_recon))).numpy()
+        melspecgrams_mean_err = tf.abs(tf.reduce_mean(melspecgrams - melspecgram_stfts_mel_recon)).numpy()
+        print('melspecgrams_err', melspecgrams_err, 'melspecgrams_mean_err', melspecgrams_mean_err)
+        assert (melspecgrams_err < 0.02)
+        assert (melspecgrams_mean_err < 0.021)
 
         stfts_mel_recon_mel_recon = spectral_ops.melspecgrams_to_stfts(melspecgram_stfts_mel_recon,
                                                                        mel2l=tf.transpose(l2mel))
-        assert (tf.abs(tf.reduce_mean(tf.abs(stfts_mel_recon) - tf.abs(stfts_mel_recon_mel_recon))).numpy() < 0.01)
+        mel_recon_err = tf.abs(tf.reduce_mean(tf.abs(stfts_mel_recon) - tf.abs(stfts_mel_recon_mel_recon))).numpy()
+        print('mel_recon_err', mel_recon_err)
+        assert (mel_recon_err < 0.001)
         waves_mel_recon = spectral_ops.stfts_to_waves(stfts_mel_recon, n_fft=n_fft, hop_length=hop_length, pad_l=pad,
-                                                      pad_r=pad, discard_dc=False)
+                                                      pad_r=pad, discard_dc=True)
         waves_mel_recon_mel_recon = spectral_ops.stfts_to_waves(stfts_mel_recon_mel_recon, n_fft=n_fft,
                                                                 hop_length=hop_length, pad_l=pad, pad_r=pad,
-                                                                discard_dc=False)
+                                                                discard_dc=True)
         assert (tf.abs(tf.reduce_mean(tf.abs(waves_mel_recon) - tf.abs(waves_mel_recon_mel_recon))).numpy() < 0.05)
 
-        # TODO: FIX NUMERICAL ERRORS!
+        # TODO: FIX/IMPROVE NUMERICAL ERRORS!
         # This performance is quite terrible, check if possible to fix the IFFT somehow...
         # Update: Cast to tf.float64 before fft/ifft to get rid of most of numerical error!
         #