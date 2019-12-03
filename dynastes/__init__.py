2408121098d36dc5ab1c480067d20692de34f585
Göran Sandström <v3qt0r@gmail.com>
2019-12-03 16:08:56 +0100

    0.3.3

--- dynastes/__init__.py
+++ dynastes/__init__.py
@@ -22,7 +22,7 @@ object_scope = {
     **regularizers.object_scope,
 }
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 # Cleanup symbols to avoid polluting namespace.
 del absolute_import
 del division
