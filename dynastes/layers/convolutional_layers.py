Masking was still broken in graph mode :( Fixed now

dynastes/layers/convolutional_layers.py
@@ -700,7 +700,7 @@ class DynastesConv2DTranspose(DynastesConv2D):
 
                 if not context.executing_eagerly():
                     # Infer the static output shape:
-                    out_shape = self._compute_output_shape(mask.shape)
+                    out_shape = self._compute_output_shape(mask.shape, mask=True)
                     outputs.set_shape(out_shape)
                 mask = (1. - outputs) < self.mask_threshold
                 mask = tf.squeeze(mask, axis=-1)
@@ -759,7 +759,7 @@ class DynastesConv2DTranspose(DynastesConv2D):
 
         return super(DynastesConv2DTranspose, self).post_process_call(outputs, training=training)
 
-    def _compute_output_shape(self, input_shape):
+    def _compute_output_shape(self, input_shape, mask=False):
         input_shape = tensor_shape.TensorShape(input_shape).as_list()
         output_shape = list(input_shape)
         if self.data_format == 'channels_first':
@@ -775,7 +775,10 @@ class DynastesConv2DTranspose(DynastesConv2D):
         else:
             out_pad_h, out_pad_w = self.output_padding
 
-        output_shape[c_axis] = self.filters
+        c_axis_shape = self.filters
+        if mask:
+            c_axis_shape = 1
+        output_shape[c_axis] = c_axis_shape
         output_shape[h_axis] = conv_utils.deconv_output_length(
             output_shape[h_axis],
             kernel_h,

tests/conv_layers_test.py
@@ -61,10 +61,15 @@ class DynastesConv1DTransposeTest(tf.test.TestCase):
             mask = to_tensor(([True] * (16 - mask_len)) + ([False] * (mask_len)))
             mask = tf.expand_dims(mask, axis=0)
             mask = tf.tile(mask, [8, 1])
-            print(mask.shape)
             layer(ts, mask=mask)
             layer.compute_mask(ts, mask=mask)
 
+            @tf.function
+            def graph_test_fn(x, mask):
+                layer(x, mask=mask)
+                layer.compute_mask(x, mask=mask)
+
+            graph_test_fn(x=ts, mask=mask)
 
     def test_specnorm(self):
         with custom_object_scope(object_scope):