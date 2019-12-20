0.5.3 - Yet another masking fix .... zzzz.....

dynastes/blocks/transformer_blocks.py
@@ -89,7 +89,7 @@ class PointWiseFeedForwardBlock(DynastesBaseLayer):
             if self.dff_layer.supports_masking:
                 mask = self.dff_layer.compute_mask(inputs, mask=mask)
             if self.out_layer.supports_masking:
-                mask = self.out_layer(inputs, mask=mask)
+                mask = self.out_layer.compute_mask(inputs, mask=mask)
         return mask
 
     def compute_output_shape(self, input_shape):
@@ -179,10 +179,7 @@ class EncoderBlock(tfkl.Layer):
             n_mask = x_mask
         else:
             n_mask = tf.math.logical_and(f_mask, res_mask)
-        if self.norm1.supports_masking:
-            x = self.norm1(f + res, training=training, mask=n_mask)
-        else:
-            x = self.norm1(f+res, training=training)
+        x, _ = cm(self.norm1, f + res, training=training, mask=n_mask)
         return x
 
     def compute_mask(self, inputs, mask=None):

tests/transformer_blocks_test.py
@@ -34,13 +34,16 @@ class EncoderBlockTest(tf.test.TestCase):
             norm = tfkl.LayerNormalization(epsilon=1e-6)
             df_net = PointWiseFeedForwardBlock(dff=dff, d_model=d_model)
             enc_block = EncoderBlock(sa_layer=sablock, norm0=norm, ffn=df_net, norm1=norm)
+
             stack = EncoderBlockStack([enc_block] * 3)
 
             test_input = tf.convert_to_tensor(normal(size=(1, length, d_model)).astype(np.float32))
             mask = to_tensor(([True] * (length - mask_len)) + ([False] * (mask_len)))
             mask = tf.expand_dims(mask, axis=0)
+            enc_out = enc_block(test_input, training=None, mask=mask)
             out = stack(test_input, training=None, mask=mask)
-
+            out_mask = enc_block.compute_mask(test_input, mask=mask)
             comp_out_shape = stack.compute_output_shape(test_input.shape)
 
             self.assertEqual(out.shape, comp_out_shape)
+            self.assertEqual(enc_out.shape, comp_out_shape)