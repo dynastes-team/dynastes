e74e8ca4b41f0910d6cf78febae82bf6c8f38402
Göran Sandström <v3qt0r@gmail.com>
2020-01-13 09:08:37 +0100

    Change ver to 0.6.0

--- dynastes/__init__.py
+++ dynastes/__init__.py
@@ -24,7 +24,7 @@ object_scope = {
     **regularizers.object_scope,
 }
 
-__version__ = "0.5.5"
+__version__ = "0.6.0"
 # Cleanup symbols to avoid polluting namespace.
 del absolute_import
 del division
