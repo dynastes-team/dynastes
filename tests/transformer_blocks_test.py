984df30266b4a5861f53fd328efb877f13319579
Göran Sandström <v3qt0r@gmail.com>
2020-01-13 10:59:48 +0100

    Fix test

--- tests/transformer_blocks_test.py
+++ tests/transformer_blocks_test.py
@@ -1,9 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.layers as tfkl
-from tensorflow.keras.utils import custom_object_scope
 
-from dynastes import object_scope
 from dynastes.blocks.attention_blocks import SelfAttentionBlock1D
 from dynastes.blocks.transformer_blocks import PointWiseFeedForwardBlock, EncoderBlock, EncoderBlockStack
 
@@ -21,29 +19,28 @@ normal = np.random.normal
 
 class EncoderBlockTest(tf.test.TestCase):
     def test_simple(self):
-        with custom_object_scope(object_scope):
-            d_model = 16
-            num_heads = 4
-            dff = 32
-            length = 4096
-            mask_len = 32
-            sablock = SelfAttentionBlock1D(d_model, d_model, num_heads=num_heads,
-                                           attention_type='PseudoBlockSparseAttention1D',
-                                           block_length=1024,
-                                           multiquery_attention=True,)
-            norm = tfkl.LayerNormalization(epsilon=1e-6)
-            df_net = PointWiseFeedForwardBlock(dff=dff, d_model=d_model)
-            enc_block = EncoderBlock(sa_layer=sablock, norm0=norm, ffn=df_net, norm1=norm)
+        d_model = 16
+        num_heads = 4
+        dff = 32
+        length = 4096
+        mask_len = 32
+        sablock = SelfAttentionBlock1D(d_model, d_model, num_heads=num_heads,
+                                       attention_type='PseudoBlockSparseAttention1D',
+                                       block_length=1024,
+                                       multiquery_attention=True, )
+        norm = tfkl.LayerNormalization(epsilon=1e-6)
+        df_net = PointWiseFeedForwardBlock(dff=dff, d_model=d_model)
+        enc_block = EncoderBlock(sa_layer=sablock, norm0=norm, ffn=df_net, norm1=norm)
 
-            stack = EncoderBlockStack([enc_block] * 3)
+        stack = EncoderBlockStack([enc_block] * 3)
 
-            test_input = tf.convert_to_tensor(normal(size=(1, length, d_model)).astype(np.float32))
-            mask = to_tensor(([True] * (length - mask_len)) + ([False] * (mask_len)))
-            mask = tf.expand_dims(mask, axis=0)
-            enc_out = enc_block(test_input, training=None, mask=mask)
-            out = stack(test_input, training=None, mask=mask)
-            out_mask = enc_block.compute_mask(test_input, mask=mask)
-            comp_out_shape = stack.compute_output_shape(test_input.shape)
+        test_input = tf.convert_to_tensor(normal(size=(1, length, d_model)).astype(np.float32))
+        mask = to_tensor(([True] * (length - mask_len)) + ([False] * (mask_len)))
+        mask = tf.expand_dims(mask, axis=0)
+        enc_out = enc_block(test_input, training=None, mask=mask)
+        out = stack(test_input, training=None, mask=mask)
+        out_mask = enc_block.compute_mask(test_input, mask=mask)
+        comp_out_shape = stack.compute_output_shape(test_input.shape)
 
-            self.assertEqual(out.shape, comp_out_shape)
-            self.assertEqual(enc_out.shape, comp_out_shape)
+        self.assertEqual(out.shape, comp_out_shape)
+        self.assertEqual(enc_out.shape, comp_out_shape)
