3812fe6a7e11115552ef5f17992eff059f5f4baf
Göran Sandström <v3qt0r@gmail.com>
2020-01-13 08:23:06 +0100

    Add mish activation from tfa

--- dynastes/activations/__init__.py
+++ dynastes/activations/__init__.py
@@ -7,7 +7,12 @@ from tensorflow.keras.utils import custom_object_scope
 from tensorflow.python.keras import activations as tf_activations
 from tensorflow.python.keras.activations import deserialize as _deserialize
 from tensorflow.python.keras.activations import serialize as _serialize
-
+tfaa = None
+try:
+    import tensorflow_addons.activations as tfaa
+except:
+    print('WARNING! TensorFlow Addons are missing!')
+    tfal = None
 
 def swish(x, beta=1):
     return x * tf.math.sigmoid(beta * x)
@@ -17,10 +22,22 @@ object_scope = {
     'swish': swish
 }
 
+mish = None
+rrelu = None
+
+if tfaa is not None:
+    mish = tfaa.mish
+    rrelu = tfaa.rrelu
+
+    object_scope['mish'] = tfaa.mish
+    object_scope['rrelu'] = tfaa.rrelu
 
 def serialize(activation):
     if activation == swish:
         return 'swish'
+    elif tfaa is not None:
+        if activation == mish:
+            return 'mish'
     return _serialize(activation)
 
 
@@ -35,5 +52,8 @@ def get(activation):
     if type(activation) == str:
         if activation == 'swish':
             return swish
+        elif tfaa is not None:
+            if activation == 'mish':
+                return mish
     with custom_object_scope(object_scope):
         return tf_activations.get(activation)
