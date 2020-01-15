a0f5fd8e534cfac0393e4797db08ff77481656be
Göran Sandström <v3qt0r@gmail.com>
2020-01-15 13:22:34 +0100

    0.6.2

--- dynastes/__init__.py
+++ dynastes/__init__.py
@@ -16,7 +16,7 @@ from dynastes import weight_normalizers
 from dynastes.core import backend
 from dynastes.probability import bijectors, bijector_partials, pseudoblocksparse_bijectors
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 # Cleanup symbols to avoid polluting namespace.
 del absolute_import
 del division
