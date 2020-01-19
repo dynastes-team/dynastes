7e2c06678f377ff3116ffb0920ede2db3fe25713
Göran Sandström <v3qt0r@gmail.com>
2020-01-19 08:02:05 +0100

    Export attention and transformer blocks

--- dynastes/blocks/__init__.py
+++ dynastes/blocks/__init__.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .attention_blocks import SelfAttentionBlock1D
+from . import attention_blocks
+from . import transformer_blocks
 
 # Cleanup symbols to avoid polluting namespace.
 del absolute_import
