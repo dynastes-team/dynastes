ea5d9dca06882ddff24b7bf52b7f3b7f7eef745a
Göran Sandström <v3qt0r@gmail.com>
2020-01-19 18:12:33 +0100

    0.6.8

--- dynastes/__init__.py
+++ dynastes/__init__.py
@@ -12,4 +12,4 @@ from dynastes import weight_normalizers
 from dynastes.core import backend
 from dynastes.probability import bijectors, bijector_partials, pseudoblocksparse_bijectors
 
-__version__ = "0.6.7"
+__version__ = "0.6.8"
